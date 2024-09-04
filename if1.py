4de40a0e8fa9531a7ddc49b634d7977f868eec89
VidhiUndavia <97037845+VidhiUndavia@users.noreply.github.com>
2024-09-04 19:19:40 +0530

    code

---
+++ if1.py
@@ -0,0 +1,7 @@
+num1=int(input("Enter the first number = "))
+num2=int(input("Enter the second number = "))
+
+if num1>num2:
+    print("First is grater then second number")
+    print(num1)
+print("Good bye")
---
+++ if2.py
@@ -0,0 +1,7 @@
+idproof=int(input("If u have id proof then enter 1 otherwise 0 = "))
+citizen=int(input("If u are indian then enter 1 or 0 = "))
+age=int(input("Enter your age = "))
+
+if age>=18 and idproof==1 and citizen==1:
+    print("You can do votting")
+print("Good bye")

37db73bd2d0a98fa6212d7653ea580fd29601146
VidhiUndavia <97037845+VidhiUndavia@users.noreply.github.com>
2024-09-05 19:13:28 +0530

    code

--- if2.py
+++ if2.py
@@ -4,4 +4,6 @@ age=int(input("Enter your age = "))
 
 if age>=18 and idproof==1 and citizen==1:
     print("You can do votting")
+else:
+    print("You are not eligible for votting")
 print("Good bye")

db0467b9f8b36c78af083eb7ab13d7d73652f8f8
VidhiUndavia <97037845+VidhiUndavia@users.noreply.github.com>
2024-09-12 19:18:19 +0530

    code

---
+++ while2.py
@@ -0,0 +1,7 @@
+#wap to take one number from the user and display its table.
+num=int(input("Enter the number = "))
+digit=1
+while digit<=10:
+    print(num," * ",digit," = ",num*digit)
+    digit+=1
+

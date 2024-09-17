0a22eb80b1cc0ebfb495da20f25eb74b8a66691c
VidhiUndavia <97037845+VidhiUndavia@users.noreply.github.com>
2024-09-17 19:24:51 +0530

    code

---
+++ for1.py
@@ -0,0 +1,11 @@
+fruit=["banana","strawberry","apple","mango"]
+
+for item in fruit:
+    print(item)
+
+emp_sal=[300000,50000,25000,25000,25000]
+bonus=5000
+total=0
+for sal in emp_sal:
+    total=sal+bonus
+    print("Salary = ",sal," + Bonus = ",bonus," Total Salary = ",total)

c2648cbc32d60a3147bd83b3b9fce90f943ee72d
VidhiUndavia <97037845+VidhiUndavia@users.noreply.github.com>
2024-10-01 19:29:06 +0530

    code

---
+++ arbitary.py
@@ -0,0 +1,10 @@
+def sayHello(*names):
+    for name in names:
+        print("Hello ",name)
+sayHello(34,"suresh",78,90)
+# output
+# D:\python_3>py arbitary.py
+# Hello  34
+# Hello  suresh
+# Hello  78
+# Hello  90
